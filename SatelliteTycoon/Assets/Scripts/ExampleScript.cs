c59956123c952c50960ab2ee3a0bb486f3798061
AdamDraheim <acdraheim@gmail.com>
2020-10-10 14:25:06 -0600

    Created example script

---
+++ SatelliteTycoon/Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+        }
+    }
+}
